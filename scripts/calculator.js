30f590463f5addfe46193b6d78232fc34f8d474c
Nolan Gajdascz <78930672+Gajdascz@users.noreply.github.com>
2023-10-05 17:04:36 -0400

    Create difference funtion and logic

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,3 +1,9 @@
+// @accumulator is the working total of the operation.
+// @current is the current index.   
+
+
 // Returns the sum of all numbers passed in as arguments.
-// @accumulator is the working sum and @current is the current index.   
-let sum = (...nums) => nums.reduce((accumulator, current) => accumulator + current, 0);
+let sum = (...nums) => nums.reduce((accumulator, current) => accumulator + current);
+
+// Returns the difference of all numbers passed in as arguments.
+let difference = (...nums) => nums.reduce((accumulator,current) => accumulator - current);
