b3844cc2d51c5deb4dffb9ba5631d63af349f3e7
Nolan Gajdascz <78930672+Gajdascz@users.noreply.github.com>
2023-10-05 18:49:52 -0400

    Start building calculator UI
    Started structuring and styling the calculator UI
    in HTML and CSS. Also added the recursive factorial
    function logic.

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,7 +1,6 @@
 // @accumulator is the working total of the operation.
 // @current is the current index.   
 
-
 // Returns the sum of all numbers passed in as arguments.
 const add = (...nums) => nums.reduce((accumulator, current) => accumulator + current);
 
@@ -16,3 +15,31 @@ const divide = (...nums) => nums.reduce((accumulator,current) => accumulator / c
 
 // Returns the power of the base and exponent passed in as arguments.
 const exponentiate = (base, exponent) => base ** exponent;
+
+// Returns the product of all numbers up to the passed argument num.
+const factorial = (num) => factorial(n * (n-1));
+
+function operate(numOne,numTwo=0,operator) {
+  if (numOne > 0){
+    switch(operator) {
+      case `+`:
+        add(numOne,numTwo);
+        break;
+      case `-`:
+        subtract(numOne,numTwo);
+        break;
+      case `*`:
+        multiply(numOne,numTwo);
+        break;
+      case `/`:
+        divide(numOne,numTwo);
+        break;
+      case `^`:
+        exponentiate(numOne,numTwo);
+        break;
+      case `!`:
+        factorial(numOne);
+        break;
+    }
+  }
+}
